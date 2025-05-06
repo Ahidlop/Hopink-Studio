820cbcfbc55528aa361495575a7e2ba0a4f0ae0c
Ahidlop <ahidlop00@gmail.com>
2025-05-06 11:12:48 +0200

    package.json Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet, Router, NavigationEnd} from '@angular/router';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { RouterModule, RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { MenuComponent } from './components/menu/menu.component';
 import { filter } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule , FooterComponent, HeaderComponent, MenuComponent],
+  imports: [RouterOutlet, RouterModule, FooterComponent, HeaderComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hopink-studio';
-  menuVisible: boolean =false;
+  menuVisible: boolean = false;
+  isBrowser: boolean;
 
   toggleMenu() {
     this.menuVisible = !this.menuVisible;
-    console.log('Menu:' ,this.menuVisible);
+    console.log('Menu:', this.menuVisible);
   }
 
-  constructor(private router: Router) {
+  constructor(private router: Router, @Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe(() => {
-        // Espera un poco para que la vista se renderice y luego sube
-        setTimeout(() => {
-          window.scrollTo({ top: 0, behavior: 'auto' }); // "auto" = más fiable que "smooth"
-        }, 100); // 100ms para asegurar render de componente
+        if (this.isBrowser) {
+          setTimeout(() => {
+            window.scrollTo({ top: 0, behavior: 'auto' });
+          }, 100);
+        }
       });
   }
 }
-  
